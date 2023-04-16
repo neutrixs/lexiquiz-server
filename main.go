6e27e1e797ee39237c157e79466602f80c2e9407
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2023-04-16 09:27:03 +0700

    serve static files

--- main.go
+++ main.go
@@ -15,10 +15,28 @@ func main() {
 		LISTEN = ":8080"
 	}
 
+	STATIC, err := env.Get("STATIC_DIR")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	INDEX, err := env.Get("INDEX_HTML")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/api/getword", api.GetWord).Methods("GET")
 	r.HandleFunc("/api/checkword", api.CheckWord).Methods("POST")
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/",http.FileServer(http.Dir(STATIC))))
+	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, INDEX)
+	})
 	
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(LISTEN, nil))
+}
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Llongfile)
 }
